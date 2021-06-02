aec976ff732354f10604d3744f0ce5f751d36aa8
BenABaron <benabaron@gmail.com>
2021-06-01 20:38:04 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import ContactList from './components/contactList/ContactList'
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userList: []
+    };
+  };
+
+  componentDidMount() {
+    fetch('https://randomuser.me/api?results=25')
+    .then(res => res.json())
+    .then(data => this.setState({
+      userList: data.results
+    }))
+    .catch(error => console.error("Error has occured. Error: ", error))
+  }
+
+  render() {
+
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+
 }
 
-export default App;
+/**
+ * use axios to get data from api
+ * 
+ */
